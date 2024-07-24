123ddb7f2c2a94f74c13ef9c1010aec1ab91e15c
hasinajy <jyhasina@gmail.com>
2024-07-24 23:19:36 +0300

    feat: Add calculateWinner function

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -33,3 +33,26 @@ function Row({ firstIndex, onCellClick, marker }) {
         </div>
     );
 }
+
+function calculateWinner(markers) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (const line of lines) {
+        const [a, b, c] = line;
+
+        if (markers[a] !== '\u00A0' && markers[a] === markers[b] && markers[a] === markers[c]) {
+            return markers[a];
+        }
+    }
+
+    return null;
+}
