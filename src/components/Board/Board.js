45ae39338bf519cf0af5c126dd374afcb1c4f1d5
hasinajy <jyhasina@gmail.com>
2024-07-24 21:56:58 +0300

    feat: Add number to each cell

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,19 +3,19 @@ import Square from "../Square/Square";
 export default function Board() {
     return (
         <div className="board">
-            <Row />
-            <Row />
-            <Row />
+            <Row firstValue={1} />
+            <Row firstValue={4} />
+            <Row firstValue={7} />
         </div>
     );
 }
 
-function Row() {
+function Row({ firstValue }) {
     return (
         <div className="board__row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={firstValue} />
+            <Square value={firstValue + 1} />
+            <Square value={firstValue + 2} />
         </div>
     );
 }
