9655df0fccdd235a2c10c8197ece65a49afb2468
hasinajy <jyhasina@gmail.com>
2024-07-25 23:09:03 +0300

    feat: Add undo move

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,16 +4,23 @@ import Board from "../Board/Board";
 export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill('\u00A0')]);
-    const currentMarker = history[history.length - 1];
+    const [currentMarker, setCurrentMarker] = useState(history[history.length - 1]);
     const moves = history.map((marker, moveIndex) => {
         const description = (moveIndex > 0) ? `Go to move #${moveIndex}` : 'Go to game start';
 
         return (
-            <li key={moveIndex}><button>{description}</button></li>
+            <li key={moveIndex}><button onClick={() => { handleMoveUndo(moveIndex) }}>{description}</button></li>
         );
     });
 
+    const handleMoveUndo = (moveIndex) => {
+        setCurrentMarker(history[moveIndex]);
+        setHistory(history.slice(0, moveIndex + 1));
+        setXIsNext((moveIndex % 2 === 0));
+    };
+
     const handlePlay = (newMarker) => {
+        setCurrentMarker(newMarker);
         setHistory([...history, newMarker]);
         setXIsNext(!xIsNext);
     };
