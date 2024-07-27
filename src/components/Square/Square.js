b1199c1b7487b5e72a48b4bc16fd899b920504d9
hasinajy <jyhasina@gmail.com>
2024-07-27 23:26:27 +0300

    chore: Remove old files

--- src/components/Square/Square.js
+++
@@ -1,9 +0,0 @@
-import './square.css'
-
-function Square({ value, onClick }) {
-    return (
-        <button className="square" onClick={onClick}>{value}</button>
-    );
-}
-
-export default Square;
