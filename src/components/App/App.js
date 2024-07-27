198088b575e28ebb798034977c7cb43f27c1fddb
hasinajy <jyhasina@gmail.com>
2024-07-27 23:17:13 +0300

    feat: Switch between X/O

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,30 +4,37 @@ import GameInfo from "../GameInfo/GameInfo";
 
 export const BoardStateContext = React.createContext();
 export const HandleCellClickContext = React.createContext();
+export const XIsNextContext = React.createContext();
 
 const WHITE_SPACE = '\u00A0';
 
 export default function App() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [boardState, setBoardState] = useState(Array(9).fill(WHITE_SPACE));
 
     const handleCellClick = useCallback((iCell) => {
+        if (boardState[iCell] !== WHITE_SPACE) return;
+
         const tempBoardState = boardState.slice();
-        tempBoardState[iCell] = 'O';
+        tempBoardState[iCell] = (xIsNext) ? 'X' : 'O';
 
         setBoardState(tempBoardState);
-    }, [boardState]);
+        setXIsNext(!xIsNext);
+    }, [boardState, xIsNext]);
 
     return (
         <BoardStateContext.Provider value={boardState}>
             <HandleCellClickContext.Provider value={handleCellClick}>
-                <h1 className="page-title">Tic-Tac-Toe</h1>
+                <XIsNextContext.Provider value={xIsNext}>
+                    <h1 className="page-title">Tic-Tac-Toe</h1>
 
-                <hr className="sep--large"></hr>
+                    <hr className="sep--large"></hr>
 
-                <div className="container">
-                    <Board />
-                    <GameInfo />
-                </div>
+                    <div className="container">
+                        <Board />
+                        <GameInfo />
+                    </div>
+                </XIsNextContext.Provider>
             </HandleCellClickContext.Provider>
         </BoardStateContext.Provider>
     );
